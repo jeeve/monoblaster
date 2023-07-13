95cbd61b3f19237dd59fe95fd71e3e5d0486f42b
jeeve <jyvaisj@gmail.com>
2023-07-13 09:59:04 +0200

    sprite

--- src/App.js
+++ src/App.js
@@ -1,48 +1,42 @@
 import { useState } from 'react';
 
-function Square({ value, onSquareClick }) {
+function Sprite( {x, y } ) {
+
+  const style = {
+    left : x,
+    top : y
+  };
+
   return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
+    <img style={style} src="player.png" />
   );
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+export default function Jeu() {
 
-  function handleClick(i) {
-    if (squares[i]) {
-      return;
+  const [x, setX] = useState(150);
+  const [y, setY] = useState(100);
+
+  function handleKeyDown(event) {
+    if (event.code == "ArrowLeft") {
+      setX(x - 5)
     }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = "X";
-    } else {
-      nextSquares[i] = "O";
+    if (event.code == "ArrowRight") {
+      setX(x + 5)
     }
-    setSquares(nextSquares)
-    setXIsNext(!xIsNext);
+    if (event.code == "ArrowDown") {
+      setY(y + 5)
+    }
+    if (event.code == "ArrowUp") {
+      setY(y - 5)
+    }
   }
 
   return (
     <>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      <div onKeyDown={handleKeyDown} className='jeu' tabIndex="0" >
+        <Sprite x={x} y={y} />
+      </div>  
     </>
   );
 }
