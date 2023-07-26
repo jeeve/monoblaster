7af5c5b934c585ab83940b92d3a20fe5a6963efa
jeeve <jyvaisj@gmail.com>
2023-07-26 13:21:08 +0200

    bomb ok

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ function Sprite({ x, y, image }) {
   const style = {
     left: x,
     top: y,
-    zIndex: image === "grass.png"|| image === "" ? 0 : image === "bomb1.png" ? 100 : 10
+    zIndex:
+      image === "grass.png" || image === ""
+        ? 0
+        : image === "bomb1.png"
+        ? 100
+        : 10,
   };
 
-  return <img style={ style } src={ image } alt="" />;
+  return <img style={style} src={image} alt="" />;
 }
 
 export default function Game() {
   const [decor, setDecor] = useState([]);
   const [player, setPlayer] = useState({ x: 150, y: 100 });
-  const [bombs, setBombs] = useState([]);
 
   useEffect(() => {
     setDecor(makeDecor());
@@ -24,7 +28,7 @@ export default function Game() {
     for (let i = 0; i < nj; i++) {
       const columns = [];
       for (let j = 0; j < ni; j++) {
-        if (i === 0 || i === nj-1 || j === 0 || j === ni-1) {
+        if (i === 0 || i === nj - 1 || j === 0 || j === ni - 1) {
           columns.push("rock.png");
         } else {
           const k = Math.random();
@@ -32,15 +36,14 @@ export default function Game() {
             columns.push("rock.png");
           } else if (k >= 0.4 && k < 0.6) {
             columns.push("brick.png");
-          }
-          else {
+          } else {
             columns.push("");
           }
         }
       }
       rows.push(columns);
     }
-    return rows;  
+    return rows;
   }
 
   function tryToGoLeft() {
@@ -54,9 +57,17 @@ export default function Game() {
       if (decor[j][i] === "" && decor[j + 1][i] === "") {
         setPlayer({ ...player, x: getRoundLessToBlock(player.x - dx) });
       } else if (player.y % 32 <= 10 && decor[j][i] === "") {
-        setPlayer({ ...player, y: getRoundMore(player.y), x: getRoundLessToBlock(player.x - dx) });
+        setPlayer({
+          ...player,
+          y: getRoundMore(player.y),
+          x: getRoundLessToBlock(player.x - dx),
+        });
       } else if (player.y % 32 >= 32 - 10 && decor[j + 1][i] === "") {
-        setPlayer({ ...player, y: getRoundLess(player.y), x: getRoundLessToBlock(player.x - dx) }); 
+        setPlayer({
+          ...player,
+          y: getRoundLess(player.y),
+          x: getRoundLessToBlock(player.x - dx),
+        });
       }
     }
   }
@@ -72,9 +83,17 @@ export default function Game() {
       if (decor[j][i] === "" && decor[j + 1][i] === "") {
         setPlayer({ ...player, x: getRoundMoreToBlock(player.x + dx) });
       } else if (player.y % 32 <= 10 && decor[j][i] === "") {
-        setPlayer({ ...player, y: getRoundMore(player.y), x: getRoundMoreToBlock(player.x + dx) });
+        setPlayer({
+          ...player,
+          y: getRoundMore(player.y),
+          x: getRoundMoreToBlock(player.x + dx),
+        });
       } else if (player.y % 32 >= 32 - 10 && decor[j + 1][i] === "") {
-        setPlayer({ ...player, y: getRoundLess(player.y), x: getRoundMoreToBlock(player.x + dx) });
+        setPlayer({
+          ...player,
+          y: getRoundLess(player.y),
+          x: getRoundMoreToBlock(player.x + dx),
+        });
       }
     }
   }
@@ -90,9 +109,17 @@ export default function Game() {
       if (decor[j][i] === "" && decor[j][i + 1] === "") {
         setPlayer({ ...player, y: getRoundLessToBlock(player.y - dx) });
       } else if (player.x % 32 <= 10 && decor[j][i] === "") {
-        setPlayer({ ...player, x: getRoundMore(player.x), y: getRoundLessToBlock(player.y - dx) });
+        setPlayer({
+          ...player,
+          x: getRoundMore(player.x),
+          y: getRoundLessToBlock(player.y - dx),
+        });
       } else if (player.x % 32 >= 32 - 10 && decor[j][i + 1] === "") {
-        setPlayer({ ...player, x: getRoundLess(player.x), y: getRoundLessToBlock(player.y - dx) });
+        setPlayer({
+          ...player,
+          x: getRoundLess(player.x),
+          y: getRoundLessToBlock(player.y - dx),
+        });
       }
     }
   }
@@ -108,19 +135,27 @@ export default function Game() {
       if (decor[j][i] === "" && decor[j][i + 1] === "") {
         setPlayer({ ...player, y: getRoundMoreToBlock(player.y + dx) });
       } else if (player.x % 32 <= 10 && decor[j][i] === "") {
-        setPlayer({ ...player, x: getRoundMore(player.x), y: getRoundMoreToBlock(player.y + dx) });
+        setPlayer({
+          ...player,
+          x: getRoundMore(player.x),
+          y: getRoundMoreToBlock(player.y + dx),
+        });
       } else if (player.x % 32 >= 32 - 10 && decor[j][i + 1] === "") {
-        setPlayer({ ...player, x: getRoundLess(player.x), y: getRoundMoreToBlock(player.y + dx) });
+        setPlayer({
+          ...player,
+          x: getRoundLess(player.x),
+          y: getRoundMoreToBlock(player.y + dx),
+        });
       }
     }
   }
 
   function dropBomb() {
     const nextDecor = [...decor];
-    const i = Math.round(player.x / 32); 
+    const i = Math.round(player.x / 32);
     const j = Math.round(player.y / 32);
     nextDecor[j][i] = "bomb1.png";
-    setBombs(nextDecor);
+    setDecor(nextDecor);
   }
 
   function handleKeyDown(event) {
@@ -143,14 +178,31 @@ export default function Game() {
 
   return (
     <div onKeyDown={handleKeyDown} className="game" tabIndex="0">
-      { decor.map((row, i) => (
-        <div key={ i }>
+      {decor.map((row, i) => (
+        <div key={i}>
           {row.map((column, j) => (
-            <Sprite key={ j } x={ j * 32 } y={ i * 32 } image={ column === "" ? "grass.png": column } />
-          )) }
+            <Sprite
+              key={j}
+              x={j * 32}
+              y={i * 32}
+              image={column === "" || column === "bomb1.png" ? "grass.png" : column}
+            />
+          ))}
         </div>
-      )) }
-      <Sprite x={ player.x } y={ player. y} image="player.png" />
+      ))}
+      <Sprite x={player.x} y={player.y} image="player.png" />
+      {decor.map((row, i) => (
+        <div key={i}>
+          {row.map((column, j) => column === "bomb1.png" ?
+            <Sprite
+              key={j}
+              x={j * 32}
+              y={i * 32}
+              image={column}
+            /> : <></>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
@@ -167,7 +219,7 @@ function getRoundLessToBlock(x) {
   if (x % 32 <= dx) {
     return getRoundMore(x);
   }
-  return x;      
+  return x;
 }
 
 function getRoundMore(x) {
@@ -178,5 +230,5 @@ function getRoundMoreToBlock(x) {
   if (x % 32 >= 32 - dx) {
     return getRoundLess(x);
   }
-  return x;      
+  return x;
 }
