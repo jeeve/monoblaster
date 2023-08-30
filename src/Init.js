32de352c0cd3e5a0f8685d9c8824cde986e439c4
jeeve <jyvaisj@gmail.com>
2023-08-30 15:52:25 +0200

    ok mobile

--- src/Init.js
+++ src/Init.js
@@ -1,6 +1,6 @@
 import * as Util from "./Util"
 
-export const ni = 20;
+export const ni = window.innerWidth <= 767 ? Math.floor(window.innerWidth / 32) : 20;
 export const nj = 15;
 export const dx = 5;
 export const tolx = 16;
