3b3679c6c34c311435d5a1a453f281f26ae7e3d3
jeeve <jyvaisj@gmail.com>
2023-08-31 11:38:32 +0200

    reborn ok

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,14 +2,16 @@ import { useState, useEffect } from "react";
 import * as Init from "../Init";
 
 export default function Player({ x, y, n, image, dead, onReborn }) {
+  const deadTime = 20;
   const [img, setImg] = useState(image);
-  const [count, setCount] = useState(6);
+  const [count, setCount] = useState(deadTime);
 
   const deltax = Math.floor(window.innerWidth / 2 - (Init.ni * 32) / 2);
   const style = {
     left: deltax + x,
     top: y,
   };
+  let interval;
 
   const startTimer = () => {
     return setInterval(() => {
@@ -21,20 +23,22 @@ export default function Player({ x, y, n, image, dead, onReborn }) {
   };
 
   useEffect(() => {
-    if (dead && count === 0) {
-      onReborn(n);
-    }
-  }, [dead, count]);
-
-  useEffect(() => {
     if (dead) {
-      const interval = startTimer();
+      interval = startTimer();
 
       return () => {
         clearInterval(interval);
       };
     }
   }, [dead]);
+
+  useEffect(() => {
+    if (dead && count === 0) {
+        clearInterval(interval);
+        setCount(deadTime);
+        onReborn(n);
+    }
+  }, [dead, count]);
 
   return <img style={style} src={"/images/" + img} alt="" />;
 }
