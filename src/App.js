52c00bf83a39ce52e9f352a09e995b97697e2130
jeeve <jyvaisj@gmail.com>
2023-09-01 13:12:12 +0200

    maj

--- src/App.js
+++ src/App.js
@@ -138,30 +138,6 @@ export default function Game() {
   };
 
   function moveRobot() {
-    const iRobot = Math.floor(robot().x / 32);
-    const jRobot = Math.floor(robot().y / 32);
-    const nRobot = Util.getIndex(iRobot, jRobot);
-
-    if (
-      Util.danger(Util.spriteLeft(nRobot), decor, fires) ||
-      Util.danger(Util.spriteRight(nRobot), decor, fires)
-    ) {
-      if (!Util.something(decor, players, robot(), Util.spriteUp(nRobot))) {
-        Engine.tryToGoUp(decor, players, robot(), setRobot);
-      } else {
-        Engine.tryToGoDown(decor, players, robot(), setRobot);
-      }
-    } else if (
-      Util.danger(Util.spriteUp(nRobot), decor, fires) ||
-      Util.danger(Util.spriteDown(nRobot), decor, fires)
-    ) {
-      if (!Util.something(decor, players, robot(), Util.spriteLeft(nRobot))) {
-        Engine.tryToGoLeft(decor, players, robot(), setRobot);
-      } else {
-        Engine.tryToGoRight(decor, players, robot(), setRobot);
-      }
-    }
-
     if (robotInertia.t > 0) {
       if (robotInertia.d === "up") {
         Engine.tryToGoUp(decor, players, robot(), setRobot);
@@ -176,18 +152,41 @@ export default function Game() {
         Engine.tryToGoRight(decor, players, robot(), setRobot);
       }
     } else {
-      const r = Math.round(Math.random() * 100);
+      const iRobot = Math.floor(robot().x / 32);
+      const jRobot = Math.floor(robot().y / 32);
+      const nRobot = Util.getIndex(iRobot, jRobot);
       const t = Math.round(Math.random() * Init.robotAgitation);
-      if (r > 0 && r <= 10) {
-        setRobotInertia({ d: "up", t: t });
-      } else if (r > 10 && r <= 20) {
-        setRobotInertia({ d: "down", t: t });
-      } else if (r > 20 && r <= 30) {
-        setRobotInertia({ d: "left", t: t });
-      } else if (r > 30 && r <= 40) {
-        setRobotInertia({ d: "right", t: t });
-      } else if (r > 40 && r <= 42) {
-        dropBomb(robot());
+      if (
+        Util.danger(Util.spriteLeft(nRobot), decor, fires) ||
+        Util.danger(Util.spriteRight(nRobot), decor, fires)
+      ) {
+        if (!Util.something(decor, players, robot(), Util.spriteUp(nRobot))) {
+          setRobotInertia({ d: "up", t: t });
+        } else {
+          setRobotInertia({ d: "down", t: t });
+        }
+      } else if (
+        Util.danger(Util.spriteUp(nRobot), decor, fires) ||
+        Util.danger(Util.spriteDown(nRobot), decor, fires)
+      ) {
+        if (!Util.something(decor, players, robot(), Util.spriteLeft(nRobot))) {
+          setRobotInertia({ d: "left", t: t });
+        } else {
+          setRobotInertia({ d: "right", t: t });
+        }
+      } else {
+        const r = Math.round(Math.random() * 100);
+        if (r > 0 && r <= 10) {
+          setRobotInertia({ d: "up", t: t });
+        } else if (r > 10 && r <= 20) {
+          setRobotInertia({ d: "down", t: t });
+        } else if (r > 20 && r <= 30) {
+          setRobotInertia({ d: "left", t: t });
+        } else if (r > 30 && r <= 40) {
+          setRobotInertia({ d: "right", t: t });
+        } else if (r > 40 && r <= 42) {
+          dropBomb(robot());
+        }
       }
     }
     setRobotInertia((old) => {
@@ -312,7 +311,7 @@ export default function Game() {
         onKeyDown={handleKeyDown}
         onKeyUp={handleKeyUp}
         tabIndex="0"
-        ref={input => input && input.focus()}
+        ref={(input) => input && input.focus()}
       >
         {decor.map((sprite, n) => (
           <Sprite
