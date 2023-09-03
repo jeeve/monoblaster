f9187936a320525f5694bc4c545eb09085db3586
jeeve <jyvaisj@gmail.com>
2023-09-03 15:35:20 +0200

    down ok

--- src/Engine.js
+++ src/Engine.js
@@ -249,5 +249,84 @@ export function tryToGoUp(decor, players, player, setPlayer) {
 }
 
 export function tryToGoDown(decor, players, player, setPlayer) {
-  setPlayer({ ...player, y: player.y + Init.dx });
+  function getSpritesArroundPlayer() {
+    let objects = [];
+    const i = Math.floor(player.x / 32);
+    const j = Math.floor(player.y / 32);
+    let sprite = decor[Util.getIndex(i - 1, j + 1)];
+    if (sprite.image !== "") objects.push({ x: sprite.x, y: sprite.y });
+    sprite = decor[Util.getIndex(i, j + 1)];
+    if (sprite.image !== "") objects.push({ x: sprite.x, y: sprite.y });
+    sprite = decor[Util.getIndex(i + 1, j + 1)];
+    if (sprite.image !== "") objects.push({ x: sprite.x, y: sprite.y });
+    players.map((p) => {
+      if (p !== player) {
+        objects.push({ x: p.x, y: p.y });
+      }
+    });
+    return objects;
+  }
+
+  function getBlocksNear() {
+    return getSpritesArroundPlayer().filter((object) => {
+      return object.y > player.y + 32 - Init.dx && object.y <= player.y + 32;
+    });
+  }
+
+  function objectAtUp(objects, o) {
+    return objects.filter((object) => {
+      if (object.y < o.y + Init.dx && object.y >= o.y) {
+        if (object.x + 32 <= o.x && object.x + 32 > o.x - 32) {
+          return true;
+        }
+      }
+      return false;
+    }).length != 0;
+  }
+
+    function objectAtDown(objects, o) {
+    return objects.filter((object) => {
+      if (object.y < o.y + Init.dx && object.y >= o.y) {
+        if (object.x >= o.x + 32 && object.x < o.x + 64) {
+          return true;
+        }
+      }
+      return false;
+    }).length != 0;
+  }
+
+  let ok = true;
+  let x = player.x;
+  let y = player.y;
+  const objects = getBlocksNear();
+  objects.forEach((object) => {
+    if (
+      (object.x + 32 > player.x && object.x + 32 < player.x + 32) ||
+      (object.x < player.x + 32 && object.x > player.x) ||
+      object.x === player.x
+    ) {
+      ok = false;
+      y = object.y - 32;
+    }
+    if (object.x < player.x + 32 && object.x > player.x + 32 - Init.tolx) {
+      ok = false;
+      if (!objectAtUp(objects, object)) {
+        ok = true;
+        x = object.x - 32;
+      }
+    }
+    if (object.x + 32 > player.x && object.x + 32 < player.x + Init.tolx) {
+      ok = false;
+      if (!objectAtDown(objects, object)) {
+        ok = true;
+        x = object.x + 32;
+      }
+    }
+  });
+  if (ok) {
+    setPlayer({ ...player, x: x, y: player.y + Init.dx });
+  } else {
+    setPlayer({ ...player, y: y });
+  }
+
 }
