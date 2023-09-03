58061ff93dd0c8df3a823464f85bfb453697078b
jeeve <jyvaisj@gmail.com>
2023-09-03 20:18:00 +0200

    engine v2

--- src/Util.js
+++ src/Util.js
@@ -12,28 +12,6 @@ export function getJ(n) {
   return Math.floor(n / Init.ni);
 }
 
-export function getRoundLess(x) {
-  return x + 32 - (x % 32);
-}
-
-export function getRoundLessToBlock(x) {
-  if (x % 32 <= Init.dx) {
-    return getRoundMore(x);
-  }
-  return x;
-}
-
-export function getRoundMore(x) {
-  return 32 * Math.floor(x / 32);
-}
-
-export function getRoundMoreToBlock(x) {
-  if (x % 32 >= 32 - Init.dx) {
-    return getRoundLess(x);
-  }
-  return x;
-}
-
 export function blockAt(decor, i, j) {
   const n = getIndex(i, j);
   return !(decor[n].image === "");
--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -2,42 +2,45 @@ import { useState, useEffect } from "react";
 import * as Init from "../Init";
 
 export default function Bomb({ x, y, n, onExplode, explode }) {
+  const [image, setImage] = useState("bomb1.png");
+  const [count, setCount] = useState(5);
 
-    const [image, setImage] = useState("bomb1.png");
-    const [count, setCount] = useState(5);
-  
-    const deltax = Math.floor(window.innerWidth / 2 - (Init.ni * 32) / 2);
-    const style = {
-      left: deltax + x,
-      top: y,
+  const deltax = Math.floor(window.innerWidth / 2 - (Init.ni * 32) / 2);
+  const style = {
+    left: deltax + x,
+    top: y,
+  };
+
+  const startTimer = () => {
+    return setInterval(() => {
+      setCount((prevCount) => prevCount - 1);
+      setImage((prevImage) =>
+        prevImage === "bomb1.png" ? "bomb2.png" : "bomb1.png"
+      );
+    }, 200);
+  };
+
+  useEffect(() => {
+    if (explode) {
+      onExplode(n);
+    }
+  }, [explode, onExplode]);
+
+  useEffect(() => {
+    const interval = startTimer();
+
+    return () => {
+      clearInterval(interval);
     };
-    
-    const startTimer = () => {
-      return setInterval(() => {
-        setCount((prevCount) => prevCount - 1);
-        setImage((prevImage) => (prevImage === "bomb1.png" ? "bomb2.png" : "bomb1.png"));
-      }, 200);
-    };
- 
-    useEffect(() => {
-      if (explode) {
-        onExplode(n);
-      }
-    }, [explode, onExplode]);
+  }, [n]);
 
-    useEffect(() => {
-      const interval = startTimer();
-  
-      return () => {
-        clearInterval(interval);
-      };
-    }, [n]);
+  useEffect(() => {
+    if (count === 0) {
+      var audio = new Audio("./sounds/explode.wav");
+      audio.play();
+      onExplode(n);
+    }
+  }, [count]);
 
-    useEffect(() => {
-      if (count === 0) {
-        onExplode(n);
-      }
-    }, [count]);
-  
-    return <img style={style} src={"/images/" + image} alt="" />;
-  }
+  return <img style={style} src={"/images/" + image} alt="" />;
+}
