d5a81a702b6c882e06916c57b9981b5f2b2e044d
jeeve <jyvaisj@gmail.com>
2023-09-07 14:21:32 +0200

    speed 20

--- src/Init.js
+++ src/Init.js
@@ -7,7 +7,7 @@ export const dx = 5;
 export const tolx = 16;
 export const robotAgitation = 10;
 export const nbBombsMax = 5;
-export const speed = 17;
+export const speed = 20;
 
 export function makeDecor(decorOK) {
     const sprites = [];
