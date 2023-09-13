32df5db0671712e139337615cf2c28d8508e5060
jeeve <jyvaisj@gmail.com>
2023-09-13 23:45:42 +0200

    up

--- src/engine.js
+++ src/engine.js
@@ -170,81 +170,140 @@ export function tryToGoRight(decor, players, player, setPlayers) {
 }
 
 export function tryToGoUp(decor, players, player, setPlayers) {
-  function getSpritesArroundPlayer() {
-    let objects = [];
-    const i = Math.floor(player.x / 32);
-    const j = Math.floor(player.y / 32);
-    let sprite = decor[util.getIndex(i - 1, j - 1)];
-    if (sprite.image !== "") objects.push({ x: sprite.x, y: sprite.y });
-    sprite = decor[util.getIndex(i, j - 1)];
-    if (sprite.image !== "") objects.push({ x: sprite.x, y: sprite.y });
-    sprite = decor[util.getIndex(i + 1, j - 1)];
-    if (sprite.image !== "") objects.push({ x: sprite.x, y: sprite.y });
+  function isInZonePlayer(sprite) {
+    return (
+      sprite.x <= player.x + 64 &&
+      sprite.x >= player.x - 64 &&
+      sprite.y + 32 >= player.y - init.dx &&
+      sprite.y + 32 <= player.y
+    );
+  }
+
+  function getObjectsArroundPlayer() {
+    const objects = decor.filter((sprite) => {
+      return sprite.image !== "" && isInZonePlayer(sprite);
+    });
     players.map((p) => {
       if (p !== player) {
-        objects.push({ x: p.x, y: p.y });
+        if (isInZonePlayer(p)) {
+          objects.push(p);
+        }
       }
+    });
+
+    objects.sort((a, b) => {
+      return a.x - b.x;
     });
     return objects;
   }
 
-  function getBlocksNear() {
-    return getSpritesArroundPlayer().filter((object) => {
-      return object.y + 32 > player.y - init.dx && object.y + 32 <= player.y;
+  function getCoupleWithSpace(objects, x) {
+    if (objects.length < 2) return null;
+    for (let i = 0; i < objects.length - 1; i++) {
+      if (Math.abs(objects[i].x - objects[i + 1].x) >= 64) {
+        if (
+          x >= objects[i].x + 32 - init.tolx &&
+          x + 32 <= objects[i + 1].x + init.tolx
+        ) {
+          return [objects[i], objects[i + 1]];
+        }
+      }
+    }
+    return null;
+  }
+
+  function getSpaceAtLeft(objects, x) {
+    const list = objects.filter((object) => {
+      return object.x >= x + 32 - init.tolx && object.x <= x + 32;
     });
+    if (list.length > 0) {
+      const obj = list[0];
+      if (
+        objects.filter((object) => {
+          return object.x + 32 <= obj.x && object.x + 32 >= obj.x - 32;
+        }).length === 0
+      ) {
+        return obj;
+      }
+    }
+    return null;
   }
 
-  function objectAtLeft(objects, o) {
-    return (
-      objects.filter((object) => {
-        if (object.x + 32 <= o.x && object.x + 32 > o.x - 32) {
-          return true;
-        }
-        return false;
-      }).length !== 0
-    );
+  function getSpaceAtRight(objects, x) {
+    const list = objects.filter((object) => {
+      return object.x + 32 >= x && object.x + 32 <= x + init.tolx;
+    });
+    if (list.length > 0) {
+      const obj = list[0];
+      if (
+        objects.filter((object) => {
+          return object.x >= obj.x + 32 && object.x <= obj.x + 32;
+        }).length === 0
+      ) {
+        return obj;
+      }
+    }
+    return null;
   }
 
-  function objectAtRight(objects, o) {
-    return (
-      objects.filter((object) => {
-        if (object.x >= o.x + 32 && object.x < o.x + 64) {
-          return true;
-        }
-        return false;
-      }).length !== 0
-    );
+  function getObjectAtTop(objects, x) {
+    const list = objects.filter((object) => {
+      return (object.x + 32 >= x && object.x + 32 <= x + 32) || (object.x <= x + 32 && object.x >= x);
+    });
+    list.sort((a, b) => {
+      return b.y - a.y;
+    });
+    if (list.length > 0) {
+      return list[0];
+    }
+    return null;
   }
 
-  let ok = true;
   let x = player.x;
   let y = player.y;
-  const objects = getBlocksNear();
-  objects.forEach((object) => {
-    if (
-      (object.x + 32 > player.x && object.x + 32 < player.x + 32) ||
-      (object.x < player.x + 32 && object.x > player.x) ||
-      object.x === player.x
-    ) {
-      ok = false;
-      y = object.y + 32;
-    }
-    if (object.x < player.x + 32 && object.x > player.x + 32 - init.tolx) {
-      ok = false;
-      if (!objectAtLeft(objects, object)) {
+  const objects = getObjectsArroundPlayer();
+  let ok =
+    objects.filter((o) => {
+      return o.x + 32 > player.x && o.x < player.x + 32;
+    }).length === 0;
+  if (!ok) {
+    const couple = getCoupleWithSpace(objects, player.x);
+    if (couple !== null) {
+      if (
+        couple[0].x + 32 <= player.x + init.tolx &&
+        couple[0].x + 32 >= player.x
+      ) {
+        ok = true;
+        x = couple[0].x + 32;
+      } else {
+        if (
+          couple[1].x >= player.x + 32 - init.tolx &&
+          couple[1].x <= player.x + 32
+        ) {
+          ok = true;
+          x = couple[1].x - 32;
+        }
+      }
+    } else {
+      let object = getSpaceAtLeft(objects, player.x);
+      if (object !== null) {
         ok = true;
         x = object.x - 32;
+      } else {
+        object = getSpaceAtRight(objects, player.x);
+        if (object !== null) {
+          ok = true;
+          x = object.x + 32;
+        } else {
+          object = getObjectAtTop(objects, player.x);
+          if (object !== null) {
+            ok = true;
+            y = object.y + 32;
+          }
+        }
       }
     }
-    if (object.x + 32 > player.x && object.x + 32 < player.x + init.tolx) {
-      ok = false;
-      if (!objectAtRight(objects, object)) {
-        ok = true;
-        x = object.x + 32;
-      }
-    }
-  });
-  if (ok) {
+  } else {
     y = player.y - init.dx;
   }
   const newPlayers = Object.assign([], players);
@@ -259,7 +318,7 @@ export function tryToGoDown(decor, players, player, setPlayers) {
       sprite.x <= player.x + 64 &&
       sprite.x >= player.x - 64 &&
       sprite.y >= player.y + 32 &&
-      sprite.y < player.y + 32 + init.dx
+      sprite.y <= player.y + 32 + init.dx
     );
   }
 
